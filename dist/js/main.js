d6a9c4ff25f25f0e86c94b3cb417922ac273bef0
Aleksandr <ashvalev78@gmail.com>
2018-12-25 04:25:38 +0300

    everything was wrong, need to remake some changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,7 +7,6 @@
  */
 
 var $backButton = $('#back_button');
-// console.log($backButton);
 
 $backButton.click((function() {
     var $loginInputForm = $('.login-input-form');
@@ -16,11 +15,28 @@ $backButton.click((function() {
     $hiddenButtonsBlock.hide();
 }));
 var $registerButton = $('.register-btn');
-// alert($registerButton);
 
 $registerButton.click((function() {
     var $loginInputForm = $('.login-input-form');
     var $hiddenButtonsBlock = $('.hidden_buttons_block');
     $loginInputForm.hide();
     $hiddenButtonsBlock.attr("style", "display: flex");
+}));
+var $popupButton1 = $('.rules_popup_button_open');
+var $popupButton2 = $('.process_popup_button_open');
+var $rulesPopup = $('.rules_popup');
+var $processPopup = $('.process_popup');
+
+$popupButton1.click((function() {
+    $rulesPopup.attr("style", "display: flex");
+}));
+
+$('.popup_cancel').click((function() {
+    $processPopup.hide();
+    $rulesPopup.hide();
+    $popupButton1.attr("style", "display: flex");
+}));
+
+$popupButton2.click((function() {
+    $processPopup.attr("style", "display: flex");
 }));
