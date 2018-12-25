b0d049a0ea99884165fabfec6654438064d312ed
Aleksandr <ashvalev78@gmail.com>
2018-12-26 01:16:47 +0300

    reuploaded

---
+++ dist/js/main.polyfills.js
@@ -0,0 +1,74 @@
+/*!
+ * project-name v0.0.1
+ * A description for your project.
+ * (c) 2018 YOUR NAME
+ * MIT License
+ * http://link-to-your-git-repo.com
+ */
+
+var $backButton = $('#back_button');
+
+$backButton.click((function() {
+    var $loginInputForm = $('.login-input-form');
+    var $hiddenButtonsBlock = $('.hidden_buttons_block');
+    $loginInputForm.show();
+    $hiddenButtonsBlock.hide();
+}));
+var $registerButton = $('.register-btn');
+
+$registerButton.click((function() {
+    var $loginInputForm = $('.login-input-form');
+    var $hiddenButtonsBlock = $('.hidden_buttons_block');
+    $loginInputForm.hide();
+    $hiddenButtonsBlock.attr("style", "display: flex");
+}));
+var $popupButton1 = $('.rules_popup_button_open');
+var $popupButton2 = $('.process_popup_button_open');
+var $rulesPopup = $('.rules_popup');
+var $processPopup = $('.process_popup');
+var checkboxRulesSwitcher = $('.agreement_input');
+var $backDimmer = $('.background_dimmer');
+var sliders = document.getElementsByClassName('agreement_input');
+
+
+$popupButton1.click((function(e) {
+    if (!$popupButton1.hasClass('slider_active')) {
+        $rulesPopup.attr("style", "display: flex");
+        $popupButton1.toggleClass("slider_active");
+        $backDimmer.show();
+    } else {
+        $popupButton1.removeClass("slider_active");
+    }
+}));
+
+$('.popup_cancel').click((function() {
+    $processPopup.hide();
+    $rulesPopup.hide();
+    $backDimmer.hide();
+    $popupButton1.attr("style", "display: flex");
+    sliders[0].checked = false;
+    sliders[1].checked = false;
+}));
+
+$('.popup_accept').click((function() {
+    $processPopup.hide();
+    $rulesPopup.hide();
+    $backDimmer.hide();
+}));
+
+$popupButton2.click((function(e) {
+    if (!$popupButton2.hasClass('slider_active')) {
+        $processPopup.attr("style", "display: flex");
+        $popupButton2.addClass("slider_active");
+        $backDimmer.show();
+    } else {
+        $popupButton2.removeClass("slider_active");
+    }
+}));
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
