794c2a4c1d254e98d557d3c433262527456df9c5
Aleksandr <ashvalev78@gmail.com>
2019-01-05 20:34:11 +0300

    images problem partically solved

--- src/js/main/map.js
+++ src/js/main/map.js
@@ -3,7 +3,7 @@ function initMap() {
     var uluru = {lat: 59.853531, lng: 30.140566};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {zoom: 14, center: uluru});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
 }
