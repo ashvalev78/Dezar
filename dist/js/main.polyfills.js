22c655f7a4d6e5ac2b9eb6ddefbaccc2f79dc96c
Aleksandr <ashvalev78@gmail.com>
2019-01-05 20:11:28 +0300

    registrations changed

--- dist/js/main.polyfills.js
+++ dist/js/main.polyfills.js
@@ -31,7 +31,7 @@ $popupBackButton.click((function() {
 }));
 function initMap() {
     // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
+    var uluru = {lat: 59.853531, lng: 30.140566};
     // The map, centered at Uluru
     var map = new google.maps.Map(
         document.getElementById('map'), {zoom: 4, center: uluru});
