15c0bb11a27efcd2031ca2d943ad248cbbb23195
Aleksandr <ashvalev78@gmail.com>
2019-01-06 18:07:53 +0300

    arrows changed, new map

--- src/js/main/map.js
+++ src/js/main/map.js
@@ -1,9 +1,21 @@
-function initMap() {
-    // The location of Uluru
-    var uluru = {lat: 59.853531, lng: 30.140566};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 14, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+// function initMap() {
+//     var uluru = {lat: 59.853531, lng: 30.140566};
+//     var map = new google.maps.Map(
+//     document.getElementById('map'), {zoom: 14, center: uluru});
+//     var marker = new google.maps.Marker({position: uluru, map: map});
+// }
+
+ymaps.ready(init);
+function init(){ 
+    // Создание карты.    
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [55.76, 37.64],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 7
+    });
 }
