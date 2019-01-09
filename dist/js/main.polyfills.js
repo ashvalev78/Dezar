467088e5f5ba9f9602b372b190e54aa2b86359c8
Aleksandr <ashvalev78@gmail.com>
2019-01-10 01:40:37 +0300

    dates deleted

--- dist/js/main.polyfills.js
+++ dist/js/main.polyfills.js
@@ -72,9 +72,13 @@ if (document.getElementsByClassName('preloader')[0] !== undefined) {
     
     // console.log('huy');
     var func = function () {
+        var f2 = function() {
+            document.getElementsByClassName('preloader')[0].classList.add('preloader__hidden');
+        }
     //     console.log($('.preloader'));
     //     $('.preloader').fadeOut(600);
         document.getElementsByClassName('preloader')[0].classList.add('hiding__preloader');
+        setTimeout(f2, 1000);
     };
     setTimeout(func, 2000);
 }
