8d541927d51249c6fb5ed3e2b8a6d63d4a98eada
Aleksandr <ashvalev78@gmail.com>
2019-01-10 03:27:41 +0300

    registration parsers added

--- src/js/main/preloader.js
+++ src/js/main/preloader.js
@@ -1,16 +1,27 @@
 
-console.log(document.getElementsByClassName('preloader')[0]);
 if (document.getElementsByClassName('preloader')[0] !== undefined) {
     
-    // console.log('huy');
     var func = function () {
         var f2 = function() {
             document.getElementsByClassName('preloader')[0].classList.add('preloader__hidden');
         }
-    //     console.log($('.preloader'));
-    //     $('.preloader').fadeOut(600);
         document.getElementsByClassName('preloader')[0].classList.add('hiding__preloader');
         setTimeout(f2, 1000);
     };
     setTimeout(func, 2000);
+
+    var enterButton = document.getElementById('bottom-block__register');
+
+    enterButton.addEventListener('click', function() {
+        var fio = document.getElementsByName('name')[0].value;
+        var phone = document.getElementsByName('phone')[0].value;
+        console.log(phone.indexOf('+'));
+        if (phone.indexOf('+') == 0) {
+            phone = phone.slice(1);
+        }
+        location.replace('/registration.html?nm=' + fio + '&tel=' + phone);
+    });
+    function avtSoc(str){
+        location.replace("?id="+str); 
+    }
 }
